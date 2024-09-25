42d02befd502f5e72b88b4988e9ff625945b112a
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-09-25 15:38:49 +0300

    fix: check visibility of compose page

--- cypress/e2e/mail/send-mail.cy.js
+++ cypress/e2e/mail/send-mail.cy.js
@@ -62,7 +62,10 @@ describe("Send health mail", () => {
     cy.login(Cypress.env("mailUsername"), Cypress.env("mailPassword"));
 
     cy.navigateToCompose();
-    cy.get('#compose_to', { timeout: 2000 }).should('be.visible').find('.form-control').type(Cypress.env("receiverMail"));
+
+    cy.url().should('include', '_task=mail&_action=compose');
+
+    cy.get('#compose_to > .col-10 > .input-group > .form-control', { timeout: 10000 }).should('be.visible').type(Cypress.env("receiverMail"));
     cy.get("#compose-subject").type(subject);
     cy.get("#composebody").type(subject);
     cy.get("#rcmbtn112").click();
