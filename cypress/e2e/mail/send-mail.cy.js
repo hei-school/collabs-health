39ff12623a6d8fa0af6ca9690eb9410a058a0927
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-09-25 14:44:02 +0300

    fix: navigateToCompose method and uncaught exception

--- cypress/e2e/mail/send-mail.cy.js
+++ cypress/e2e/mail/send-mail.cy.js
@@ -31,15 +31,38 @@ Cypress.Commands.add(
   },
 );
 
+Cypress.Commands.add("navigateToCompose", () => {
+  const composeUrl = `${Cypress.env("mailUrl")}/?_task=mail&_action=compose`;
+  cy.visit(composeUrl);
+  
+  // Check for error box
+  cy.get('body').then($body => {
+    if ($body.find('.boxerror').length > 0) {
+      // If error box is present, wait and try again
+      cy.wait(5000); // Wait for 5 seconds
+      cy.visit(composeUrl);
+    }
+  });
+  
+  // Ensure compose page is loaded
+  cy.get('#compose_to > .col-10 > .input-group > .form-control').should('be.visible');
+});
+
 describe("Send health mail", () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from failing the test
+    console.log('Uncaught exception:', err.message)
+    return false
+  })
+
   it("should send health mail", () => {
     const currentTime = new Date().toLocaleString();
     const subject = `Health check - ${currentTime}`;
 
     cy.login(Cypress.env("mailUsername"), Cypress.env("mailPassword"));
 
-    cy.visit(`${Cypress.env("mailUrl")}/?_task=mail&_action=compose`);
-    cy.get('#compose_to').type(Cypress.env("receiverMail"));
+    cy.navigateToCompose();
+    cy.get('#compose_to > .col-10 > .input-group > .form-control').type(Cypress.env("receiverMail"));
     cy.get("#compose-subject").type(subject);
     cy.get("#composebody").type(subject);
     cy.get("#rcmbtn112").click();
