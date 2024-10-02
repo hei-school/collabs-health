cd860b5c18ee006ec79bb180a989c2fd680b3386
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-10-02 15:38:42 +0300

    fix: add more timeout

--- cypress/e2e/mail/send-mail.cy.js
+++ cypress/e2e/mail/send-mail.cy.js
@@ -7,22 +7,20 @@ Cypress.Commands.add("login", (username, password) => {
 });
 
 Cypress.Commands.add("extractSubjectsFromRows", (numRows = 10) => {
-  return cy.get("#messagelist-content").then(($messageList) => {
-    cy.wait(3000);
+  cy.wait(5000); // Attendre le chargement des messages
 
-    //TODO: It works only when user have minimum one mail on inbox
-    return cy.get('tr[id^="rcmrow"]').then(($rows) => {
-      const extractedSubjects = [];
-      $rows.slice(0, numRows).each((index, row) => {
-        cy.wrap(row)
-          .find("td.subject > .subject > a > span")
-          .invoke("text")
-          .then((text) => {
-            extractedSubjects.push(text.trim());
-          });
-      });
-      return cy.wrap(extractedSubjects);
+  //TODO: It needs an email on inbox to create the element rcmrow
+  return cy.get('tr[id^="rcmrow"]').then(($rows) => {
+    const extractedSubjects = [];
+    $rows.slice(0, numRows).each((index, row) => {
+      cy.wrap(row)
+        .find("td.subject > .subject > a > span")
+        .invoke("text")
+        .then((text) => {
+          extractedSubjects.push(text.trim());
+        });
     });
+    return cy.wrap(extractedSubjects);
   });
 });
 
@@ -36,11 +34,54 @@ Cypress.Commands.add(
   },
 );
 
-describe("Check error mail", () => {
-  const errorSubject = "Undelivered Mail Returned to Sender";
+Cypress.Commands.add("navigateToCompose", () => {
+  const composeUrl = `${Cypress.env("mailUrl")}/?_task=mail&_action=compose`;
+  cy.visit(composeUrl);
 
-  it("should not have error mail", () => {
+  // Handle potential redirects
+  cy.url().should("include", "_task=mail&_action=compose");
+
+  // Wait for the page to stabilize
+  cy.get("body").should("not.have.class", "loading");
+
+  // Check for error box and retry if present
+  cy.get("body").then(($body) => {
+    if ($body.find(".boxerror").length > 0) {
+      cy.wait(5000); // Wait for 5 seconds
+      cy.visit(composeUrl);
+      cy.url().should("include", "_task=mail&_action=compose");
+      cy.get("body").should("not.have.class", "loading");
+    }
+  });
+
+  // Ensure the compose form is visible
+  cy.get("#compose-content").should("be.visible");
+});
+
+describe("Send health mail", () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+    console.log("Uncaught exception:", err.message);
+    return false;
+  });
+
+  it("should send health mail", () => {
+    const currentTime = new Date().toLocaleString();
+    const subject = `Health check - ${currentTime}`;
+
     cy.login(Cypress.env("mailUsername"), Cypress.env("mailPassword"));
-    cy.checkSubjectInMessages(errorSubject, 10, false);
+
+    cy.navigateToCompose();
+
+    cy.get("#compose_to", { timeout: 20000 })
+      .should("be.visible")
+      .type(Cypress.env("receiverMail"));
+    cy.get("#compose-subject").should("be.visible").type(subject);
+    cy.get("#composebody").should("be.visible").type(subject);
+    cy.get("#rcmbtn112").click();
+
+    cy.visit(`${Cypress.env("mailUrl")}/?_task=mail&_mbox=Sent`);
+    cy.get('tr[id^="rcmrow"]').should("be.visible");
+    cy.checkSubjectInMessages(subject, 5, true);
   });
 });
